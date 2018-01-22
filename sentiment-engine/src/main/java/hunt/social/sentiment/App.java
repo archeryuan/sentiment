60b4b098a96bf40fd88eec3b27d3bf3cff4521ff
Archer Yuan <archer.yuan@gmail.com>
2018-01-22 17:34:05 +0800

    add sentiment project

---
+++ sentiment-engine/src/main/java/hunt/social/sentiment/App.java
@@ -0,0 +1,13 @@
+package hunt.social.sentiment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
